4f10a693134aa219068265436b7dea9e84a4e701
EsinShadrach <esinshadrachrafe@gmail.com>
2024-04-16 23:23:24 +0100

    Added Jay Date Picker

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_playground/main.dart";
 import "package:flutter_playground/screens/animated_list/animated_list_example.dart";
+import "package:flutter_playground/screens/jay/date_picker.dart";
 import "package:flutter_playground/screens/page_animation/page_animation.dart";
 
 mixin AppRoutes {
@@ -14,4 +15,5 @@ Map<String, WidgetBuilder> routes() => {
       AppRoutes.home: (context) => const HomePage(),
       AppRoutes.pageAnimation: (context) => const PageAnimation(),
       AppRoutes.animatdList: (context) => const AnimatedListScreen(),
+      AppRoutes.jay: (context) => const JayDatePicker(),
     };
