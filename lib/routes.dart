7e2aaede1e0ad487ac976dcf8133446602c5f32b
EsinShadrach <esinshadrachrafe@gmail.com>
2024-04-24 16:48:43 +0100

    Added hero transtion

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_playground/main.dart";
 import "package:flutter_playground/screens/animated_list/animated_list_example.dart";
+import "package:flutter_playground/screens/examples/hero_widget/hero_widget_example.dart";
 import "package:flutter_playground/screens/examples/overlay_example/overlay_example_screen.dart";
 import "package:flutter_playground/screens/examples/scale_down_btn/scale_down_btn_example.dart";
 import "package:flutter_playground/screens/page_animation/page_animation.dart";
@@ -11,6 +12,7 @@ mixin AppRoutes {
   static const String animatdList = "/animated-list";
   static const String scaleDownOnPressed = "/scale-down-on-pressed-btn";
   static const String overlayExample = "/overlay-example";
+  static const String heroWidget = "/hero-widget";
 }
 
 Map<String, WidgetBuilder> routes() => {
@@ -19,4 +21,5 @@ Map<String, WidgetBuilder> routes() => {
       AppRoutes.animatdList: (context) => const AnimatedListScreen(),
       AppRoutes.scaleDownOnPressed: (context) => const ScaleDownButton(),
       AppRoutes.overlayExample: (context) => const OverlayScreenExample(),
+      AppRoutes.heroWidget: (context) => const HeroWidgetScreen(),
     };
---
+++ lib/screens/examples/hero_widget/hero_widget_example.dart
@@ -0,0 +1,97 @@
+import "dart:math";
+
+import "package:flutter/material.dart";
+
+class HeroWidgetScreen extends StatelessWidget {
+  const HeroWidgetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hero Screen - 1"),
+      ),
+      body: Center(
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          margin: const EdgeInsets.all(10),
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SecondScreen(),
+                ),
+              );
+            },
+            child: Hero(
+              tag: "peick",
+              flightShuttleBuilder: (flightContext, animation, flightDirection,
+                  fromHeroContext, toHeroContext) {
+                final customAnimation = Tween<double>(
+                  begin: 0,
+                  end: 2,
+                ).animate(animation);
+
+                return AnimatedBuilder(
+                  animation: customAnimation,
+                  builder: (context, child) {
+                    return Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.003)
+                        ..rotateX(customAnimation.value * pi),
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        "assets/peick.webp",
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Image.asset(
+                "assets/peick.webp",
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hero Screen - 2"),
+      ),
+      body: Container(
+        clipBehavior: Clip.hardEdge,
+        margin: const EdgeInsets.all(10),
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: InkWell(
+          onTap: () {
+            //
+          },
+          child: Hero(
+            tag: "peick",
+            child: Image.asset(
+              "assets/peick.webp",
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
